6838a83b24975c88c236ee6b705f6a428a0afb8c
Blake Bottum <blake@platypuslabs.llc>
2020-11-29 21:33:24 -0600

    MLFQ

--- kernel/fcfs.h
+++ kernel/fcfs.h
@@ -1,16 +1,16 @@
-#ifndef _fsfs_h_
-#define _fsfs_h_
+#ifndef _fcfs_h_
+#define _fcfs_h_
 
 #include "scheduler.h"
 
-class FSFS : public Scheduler {
+class FCFS : public Scheduler {
 
     Queue<gheith::TCB,InterruptSafeLock> readyQ{};
 
 public:
-    FSFS() {}
+    FCFS() {}
 
-    ~FSFS() {}
+    ~FCFS() {}
 
     bool schedule(gheith::TCB* thread, Source source) {
 	    readyQ.add(thread);
--- kernel/impls.h
+++ kernel/impls.h
@@ -1,7 +1,7 @@
 #ifndef _impls_h_
 #define _impls_h_
 
-#include "fsfs.h"
+#include "fcfs.h"
 #include "mlfq.h"
 
 #endif
