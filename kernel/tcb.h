d24184f49889a8c2b2467bfb0d7f251ff5f4c760
Tyler Roberts <troberts@cs.utexas.edu>
2020-11-30 19:55:32 -0600

    tcb change somehow

--- kernel/tcb.h
+++ kernel/tcb.h
@@ -26,7 +26,7 @@ namespace gheith {
         TCB* next;
         SaveArea saveArea;
         Shared<PCB> pcb;
-        uint32_t scrap;
+        void* scrap = nullptr;
 
         TCB(bool isIdle) : isIdle(isIdle), id(next_id.fetch_add(1)) {
             saveArea.tcb = this;
